c6386cf353dbf22569f6db4021815270cbf09c5b
Giacomo Melacini <gmelacini@unibz.it>
2021-02-25 19:03:48 +0100

    Updated reader

--- utils/load_csv.py
+++ utils/load_csv.py
@@ -7,9 +7,23 @@ def import_csv(filename, delimiter=','):
     with open(filename) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=delimiter)
 
-        for row in csv_reader:
-            data.append(row)
+        count_row = 0
+        street_number = 0
+        streets = []
+        paths = []
+        first_line = None
 
-    return data
+        for row in csv_reader:
+            if count_row == 0:
+                first_line = row
+                street_number = row[2]
+            elif count_row < street_number:
+                streets.append(row)
+            else:
+                paths.append(row)
+
+            count_row += 1
+
+    return first_line, streets, paths
 
 
